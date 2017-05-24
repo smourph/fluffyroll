206a637fc2c262a68e02a21510a87a5518f61f9d
Julien De Conti <julien.de.conti@gmail.com>
2017-05-24 22:52:24 +0200

    [Composer] Install symfony/assetic-bundle (v2.8.1)

--- website/app/AppKernel.php
+++ website/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FluffyRollBundle\FluffyRollBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

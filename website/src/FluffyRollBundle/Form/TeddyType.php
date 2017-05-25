3356d04133b07908ab2b344ec12245352324ec1e
Julien De Conti <julien.de.conti@gmail.com>
2017-05-25 04:01:04 +0200

    [FluffyRoll][New] Design page

--- website/src/FluffyRollBundle/Form/TeddyType.php
+++ website/src/FluffyRollBundle/Form/TeddyType.php
@@ -5,6 +5,7 @@ namespace FluffyRollBundle\Form;
 use FluffyRollBundle\Entity\Teddy;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,15 @@ class TeddyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ]
+            )
             ->add(
                 'image',
                 FileType::class,
@@ -25,6 +34,7 @@ class TeddyType extends AbstractType
                     'attr' => [
                         'accept' => 'image/*',
                         'capture' => 'camera',
+                        'class' => 'form-control',
                     ],
                 ]
             );

72dc2b314d0b88b6470266ee791988c3302d4214
Julien De Conti <julien.de.conti@gmail.com>
2017-05-25 16:42:49 +0200

    [FluffyRoll][FileUploader] Sanitize filename before saving

--- website/src/FluffyRollBundle/DependencyInjection/Utils/FileUploader.php
+++ website/src/FluffyRollBundle/DependencyInjection/Utils/FileUploader.php
@@ -32,6 +32,7 @@ class FileUploader
      */
     public function upload(UploadedFile $file, string $prefix = ''): string
     {
+        $prefix = $this->sanitize($prefix, true, true);
         $fileName = $prefix.'-'.md5(uniqid()).'.'.$file->guessExtension();
         $file->move($this->_targetDir, $fileName);
 
@@ -53,5 +54,27 @@ class FileUploader
     public function getFile(string $fileName)
     {
         return new File($this->_targetDir.$fileName);
+    }
+
+    /**
+     * @param $string
+     * @param bool $lowercase
+     * @param bool $fullReplace
+     * @return string
+     */
+    private function sanitize($string, $lowercase = true, $fullReplace = false) {
+        $strip = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+            "}", "\\", "|", ";", ":", "\"", "'", "", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+            "â€”", "â€“", ",", "<", ".", ">", "/", "?"];
+
+        $clean = trim(str_replace($strip, "", strip_tags($string)));
+        $clean = preg_replace('/\s+/', "_", $clean);
+        $clean = ($fullReplace) ? preg_replace("/[^a-zA-Z0-9\-_]/", "", $clean) : $clean;
+
+        return ($lowercase) ?
+            (function_exists('mb_strtolower')) ?
+                mb_strtolower($clean, 'UTF-8') :
+                strtolower($clean) :
+            $clean;
     }
 }
